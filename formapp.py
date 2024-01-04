fe6d425c96c9371698a2349cc492fccd5fd6f750
Sandeep <smongaphd6@gmail.com>
2024-01-04 07:55:54 +0530

    fifth

--- formapp.py
+++ formapp.py
@@ -1,8 +1,12 @@
 import streamlit as st
 with st.form("my_form"):
-    x=st.text_input("Enter the number-1 ")
-    y=st.text_input("Enter the number-2")
+    x=st.text_input("Enter the number-1 ",value=0)
+    y=st.text_input("Enter the number-2",value=0)
+    st.checkbox("Male")
+    st.checkbox("Female")
+    color=st.color_picker("Select Color")
     #st.write(x)
     submit=st.form_submit_button()
     if submit:
         st.write("Addition is ",int(x)+int(y))
+        st.write("Selected Color is ",color)
